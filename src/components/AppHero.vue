16116a37c872e7d294fba3bbd50d9fc9b0c498a4
LucaConiglio <lucaconiglio.92@gmail.com>
2022-12-01 15:11:39 +0100

    aggiunto title e jumbotron

---
+++ src/components/AppHero.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <img :src="srcImmagine" alt="uomo con la bici in campagna">
+  </div>
+</template>
+
+<script>
+export default {
+  
+  data() {
+    return {
+      
+      srcImmagine : "https://redazione-style.corriere.it/wp-content/uploads/2020/05/bici-pexels.jpg"
+
+    }
+  },
+}
+</script>
+
+<style>
+img{
+  display: block;
+  width: 100%;
+}
+</style>
+
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{primoTitolo}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  
+  data() {
+    return {
+      primoTitolo : "La mia prima app con Vite!"
+    }
+  },
+
+}
+</script>
+
+<style>
+h1{
+  text-align: center;
+  color: red;
+  padding-top: 2rem;
+}
+</style>
